55e28675142ee5c3073c3dfed2824a82b200b249
koteng915 <koteng6535@gmail.com>
2022-03-05 20:28:54 +0800

    initial camera capture video

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import cv2
-import cvzone
+
+import HandTrackingModule
+import PoseModule
 
 cap= cv2.VideoCapture(0) #0 =laptop camera capture video
+detector = HandTrackingModule.HandDetector(maxHands=1, detectionCon=0.7)
+Pose = PoseModule.PoseDetector
+
 
 while True:
     success, img = cap.read()
+    cv2.imshow("image", img)  # video window's name
+    hand = detector.findHands(img) #use the cvzone to find hand
+    lmList, bbox = Pose.findPosition(img)
 
-    cv2.imshow("image", img) #video window's name
+
     cv2.waitKey(1) #delay time
