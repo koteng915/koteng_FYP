bb0b3959f939129e5fc0eb687c7f92837523b5ca
koteng915 <koteng6535@gmail.com>
2022-03-05 17:37:54 +0800

    initial camera capture video

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import cv2
+import cvzone
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+cap= cv2.VideoCapture(0) #0 =laptop camera capture video
 
+while True:
+    success, img = cap.read()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    cv2.imshow("image", img) #video window's name
+    cv2.waitKey(1) #delay time
